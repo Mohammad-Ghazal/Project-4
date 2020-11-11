f5d7078d6c3862d37a0bcea949d509302a51bb1d
Mohammad-Ghazal <moh.g.ghazal@gmail.com>
2020-11-11 19:03:57 +0200

    add basic featurs

---
+++ back-end/controllers/main-controller.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+//tack what ecxported from Router
+const mainRouter = express.Router();
+
+
+const users = [
+{id:1,name:'ghazal',birth:'12/4/1995'},
+{id:2,name:'Jozaa',birth:'21/7/1994'},
+{id:3,name:'ali',birth:'15/5/1991'},
+{id:4,name:'khamil',birth:'22/9/1985'},
+{id:5,name:'zara',birth:'10/1/1999'},
+
+]
+
+const getAllUsers=(req,res)=>{
+console.log('getAllUsers called')
+    //res.json(`users :  ${users}`)
+    res.json(users)
+}
+
+const addNewUser=(req,res)=>{
+    console.log(req.body)
+
+    //req.args.
+   const newUser={id:users.length+1,name:req.body.name,birth:req.body.birth}
+   users.push(newUser)
+        res.json(newUser.name+' is now a user  ')
+    }
+
+module.exports={getAllUsers,addNewUser};
--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -2,12 +2,21 @@
 const express = require('express'); 
 //tack what ecxported from Router
 const mainRouter = express.Router();
-
+const {getAllUsers,addNewUser}=require('../controllers/main-controller')
 
 //to call server >> you have to ::
 //http://localhost:PORT/enpoint path
-mainRouter.get('/', (req, res) => {
-  res.json('HELLO WORLD');
+mainRouter.get('/', (req,res) => {
+  res.json('wellcome to ghzal project four');
 });
 
+
+mainRouter.get('/getAllUsers',(req,res)=>{
+getAllUsers(req,res);
+
+})
+mainRouter.post('/addNewUser',(req,res)=>{
+  addNewUser(req,res);
+  
+  })
 module.exports = mainRouter;

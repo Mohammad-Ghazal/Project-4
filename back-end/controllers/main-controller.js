826fd8cdd536d79ea751f7bc7cf22d40b2896069
Mohammad-Ghazal <moh.g.ghazal@gmail.com>
2020-11-12 21:09:11 +0200

    DeleteArtivcleByAuther edit using map

--- back-end/controllers/main-controller.js
+++ back-end/controllers/main-controller.js
@@ -1,3 +1,4 @@
+const { compareSync } = require("bcrypt");
 const express = require("express");
 //tack what ecxported from Router
 const mainRouter = express.Router();
@@ -13,7 +14,7 @@ const articles_data = [
     id: 4,
     title: "how to studey react",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    author: "abd",
+    author: "jouza",
   },
   {
     id: 7,
@@ -30,14 +31,6 @@ const getAllArticles = (req, res) => {
 };
 
 const createNewArticle = (req, res) => {
-  // const newArtical = {
-  //   id: posts.length + 1,
-  //   title: req.body.title,
-  //   description: req.body.description,
-  //   author:req.body.author
-
-  // };
-
   console.log("createNewArticle CALLED");
   // console.log('REQ: ',req)
   console.log("REQ.BODY: ", req.body);
@@ -53,65 +46,74 @@ const changeArticleTitleByID = (req, res) => {
   // console.log("areq.params.id: ", req.params.id);
   // console.log("articles_data[req.params.id-1]: ", articles_data[req.params.id-1]);
 
-//   articles_data[req.params.id - 1].title = req.params.newTitle;
-//   res.json(articles_data);
-/*
+  //   articles_data[req.params.id - 1].title = req.params.newTitle;
+  //   res.json(articles_data);
+  /*
 id: 1,
 title: "eat fried chicken",
 description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
 author: "jouza",
 */
-console.log(req.params.id);
-let id=req.params.id;
-//   let art1 =articles_data.filter((art,index)=>art.id==req.params.id);//req.params.id
-//  // art1.title=req.params.newTitle;
-//   aer1[0].title=req.params.newTitle;
-// console.log(  articles_data[1])
-//   //art1.title=req.params.newTitle;
-//   res.json(articles_data);
+  console.log(req.params.id);
+  let id = req.params.id;
+  //   let art1 =articles_data.filter((art,index)=>art.id==req.params.id);//req.params.id
+  //  // art1.title=req.params.newTitle;
+  //   aer1[0].title=req.params.newTitle;
+  // console.log(  articles_data[1])
+  //   //art1.title=req.params.newTitle;
+  //   res.json(articles_data);
 
-
-articles_data.map((article,index) => {
-    if(article.id==req.params.id)
-    articles_data[index].title=req.params.newTitle
-
-});
-res.json(articles_data)
+  articles_data.map((article, index) => {
+    if (article.id == req.params.id)
+      articles_data[index].title = req.params.newTitle;
+  });
+  res.json(articles_data);
 };
 const changeArticleAuthorById = (req, res) => {
-console.log("changeArticleAuthorById CALLED");
-articles_data.map((article,index) => {
-    if(article.id==req.params.id)
-    articles_data[index].author=req.body.newAuthor
-
-});
-res.json(articles_data)
+  console.log("changeArticleAuthorById CALLED");
+  articles_data.map((article, index) => {
+    if (article.id == req.params.id)
+      // articles_data[index].author=req.body.newAuthor
+      article.author = req.body.newAuthor;
+  });
+  res.json(articles_data);
 };
 
 const deleteArticleById = (req, res) => {
-console.log("deleteArticleById CALLED");
+  console.log("deleteArticleById CALLED");
 
-//articles_data=articles_data.filter(article =>article.id!=req.params.id)//can't change const like this
-//articles_data.filter(article =>article.id!=req.params.id) 
+  //articles_data=articles_data.filter(article =>article.id!=req.params.id)//can't change const like this
+  //articles_data.filter(article =>article.id!=req.params.id)
 
-articles_data.map((article,index) => {
-  if(article.id==req.params.id)
-  articles_data.splice(index, 1)
-});
+  articles_data.map((article, index) => {
+    if (article.id == req.params.id) articles_data.splice(index, 1);
+  });
 
-res.json(articles_data)
+  res.json(articles_data);
 };
 
 const deleteArticleByAuthor = (req, res) => {
-console.log("deleteArticleByAuthor CALLED");
+  console.log("deleteArticleByAuthor CALLED");
 
-articles_data.map((article,index) => {
-  if(article.author==req.body.author)
-    articles_data.splice(index, 1)
-});
+  //   articles_data1 = articles_data.filter((art) => {
+  //     if (art.author !== req.body.author) return art;
+  //   });
+  //   res.json(articles_data1);
 
-res.json(articles_data)
+  let haveToDeleteIndex = [];
+  articles_data.map((article, index) => {
+    if (article.author === req.body.author) {
+      haveToDeleteIndex.unshift(index);
+    }
+  });
+
+  haveToDeleteIndex.forEach((element) => {
+    articles_data.splice(element, 1);
+  });
+
+  res.json(articles_data);
 };
+
 module.exports = {
   // "getAllArticles":getAllArticles,
   getAllArticles,
@@ -119,5 +121,23 @@ module.exports = {
   changeArticleTitleByID,
   changeArticleAuthorById,
   deleteArticleById,
-  deleteArticleByAuthor
+  deleteArticleByAuthor,
 };
+
+// function User(firstName,lastName,birth){
+
+//   this.firstName=firstName;
+//   this.lastName= lastName;
+//   this.birth =new Date(birth);
+//   //  this.getFullName=function(){return `${this.firstName} ${this.lastName}`}
+
+//   }
+//   //best practic to not load alot of data
+//   User.prototype.getFullName=function(){return `${this.firstName} ${this.lastName}`}
+//   User.prototype.getBirthYear=function(){return this.birth.getFullYear()}
+
+//   const user1=new User('John','Doe','4-3-1970');
+//   const user2=new User('John','Doe','12-29-1988');
+
+//   console.log('here a birth year for u1 : ',user1.getFullName());
+//   console.log('here a birth year for u2 : ',user2.getBirthYear());
